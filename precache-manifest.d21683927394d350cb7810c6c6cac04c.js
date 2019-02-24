f137c1b589c6316b2af647afba63726081232501
Bel Imperial <imperial.bel@gmail.com>
2019-02-24 19:24:32 +0800

    Updates

---
+++ precache-manifest.d21683927394d350cb7810c6c6cac04c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5a516bccde9cdaa0e0a",
+    "url": "/robofriends-react/static/css/main.ac2d9cce.chunk.css"
+  },
+  {
+    "revision": "c5a516bccde9cdaa0e0a",
+    "url": "/robofriends-react/static/js/main.c5a516bc.chunk.js"
+  },
+  {
+    "revision": "076fe318fc01ecb0ad8d",
+    "url": "/robofriends-react/static/js/runtime~main.076fe318.js"
+  },
+  {
+    "revision": "b9508b1ecd2f5630e9a6",
+    "url": "/robofriends-react/static/css/2.542effd6.chunk.css"
+  },
+  {
+    "revision": "b9508b1ecd2f5630e9a6",
+    "url": "/robofriends-react/static/js/2.b9508b1e.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-react/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "56b33451980660f03edd38139c7a9084",
+    "url": "/robofriends-react/index.html"
+  }
+];
